c4f93109d919a76e6eb21ae59f9c45f7cfe6aad5
Abror <komalov@me.com>
2023-05-23 16:15:34 +0500

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import type { AppProps } from 'next/app';
 import { Poppins } from 'next/font/google';
 
 import { DarkModeProvider } from '@/components/darkModeProvider';
-
+import { Analytics } from '@vercel/analytics/react';
 import '@/styles/styles.css';
 
 const poppins = Poppins({
@@ -22,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 <Provider store={store}>
                     <DarkModeProvider>
                         <Component {...pageProps} />
+                        <Analytics />
                     </DarkModeProvider>
                 </Provider>
             </main>
